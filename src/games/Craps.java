9ed660a2320636013709d629b27515c86699c263
txngUI <tanguy.davidpro85@gmail.com>
2024-12-10 21:49:25 +0100

    fix win Craps.java

--- src/games/Craps.java
+++ src/games/Craps.java
@@ -1,4 +1,112 @@
 package games;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Craps {
+    // Constante de la classe
+    private final int NB_DES = 2;
+
+    // Attributs privés de la classe
+    private final Dice[] des = new Dice[NB_DES];
+    private List<Integer> valuesWon; // Liste des valeurs gagnantes
+    private List<Integer> valuesLose; // Liste des valeurs perdantes
+
+    /**
+     * Constructeur de la classe Craps
+     */
+    public Craps() {
+        initialize(); // Initialisation des attributs
+    }
+
+    /**
+     * Méthode permettant de jouer au jeu de Craps
+     * @return le nombre de tours joués
+     */
+    public int play() {
+        // Initialisation des variables
+        int score = 0;
+        int point = 0;
+        boolean gameOver = false;
+
+        // Boucle de jeu
+        while (!gameOver) {
+
+            // initialisation de la valeur des dés
+            int sum = 0;
+
+            // Lancer des dés
+            for (int i = 0; i < NB_DES; i++) {
+                des[i].roll();
+                System.out.println("Lancé du dé " + (i + 1) + " : " + des[i].get_value());
+                sum += des[i].get_value();
+            }
+
+            // Affichage du total
+            System.out.printf("Vous avez fait un total de %d\n", sum);
+
+            // Incrémentation du score
+            score++;
+
+            // Étape 1 : Come-Out Roll
+            if (point == 0) {
+                // Vérification des valeurs gagnantes et perdantes
+                if (valuesLose.contains(sum)) {
+                    System.out.println("Tu as perdu !");
+                    score = -1;
+                    gameOver = true;
+                } else if (valuesWon.contains(sum)) {
+                    System.out.println("Tu as gagné !");
+                    gameOver = true;
+                } else { // Établissement du point
+                    point = sum;
+                    System.out.println("Tu as établi un point : " + point);
+                }
+            }
+            // Étape 2 : Phase de point
+            else {
+                // Vérification des valeurs gagnantes et perdantes
+                if (sum == point) {
+                    System.out.println("Tu as gagné en refaisant ton point !");
+                    gameOver = true;
+                } else if (sum == 7) {
+                    System.out.println("Tu as perdu en obtenant un 7 avant de faire ton point !");
+                    gameOver = true;
+                    score = -1;
+                }
+            }
+
+            // Demande de continuer
+            if (!gameOver) {
+                System.out.println("Souhaitez-vous continuer ? (o/n)");
+
+                if (Lire.c() == 'n') {
+                    gameOver = true;
+                }
+            }
+        }
+
+        // Retour du score
+        return score;
+    }
+
+    /**
+     * Méthode permettant d'initialiser les attributs de la classe
+     */
+    private void initialize() {
+        // Initialisation des dés
+        for (int i = 0; i < NB_DES; i++) {
+            des[i] = new Dice(6);
+        }
+
+        // Initialisation des valeurs gagnantes et perdantes
+        valuesLose = new ArrayList<>();
+        valuesLose.add(2);
+        valuesLose.add(3);
+        valuesLose.add(12);
+
+        valuesWon = new ArrayList<>();
+        valuesWon.add(7);
+        valuesWon.add(11);
+    }
 }
--- src/games/Game421.java
+++ src/games/Game421.java
@@ -1,4 +1,108 @@
 package games;
 
 public class Game421 {
+    // Constantes de la classe
+    private static final int NB_DES = 3;
+    private static final int NB_FACES = 6;
+
+    // Attributs de la classe
+    private final int faces;
+    private final Dice[] des = new Dice[NB_DES];
+
+    /**
+     * Constructeur de la classe
+     * @param faces : nombre de faces des dés
+     */
+    public Game421(int faces) {
+        this.faces = faces;
+    }
+
+    /**
+     * Constructeur de la classe par défaut
+     */
+    public Game421() {
+        this(NB_FACES);
+    }
+
+    /**
+     * Méthode pour jouer au jeu 421
+     * @return le score obtenu
+     */
+    public int play() {
+
+        // Initialisation des dés
+        initialize();
+
+        // Variables locales
+        int score = 0;
+        int locked_dices = 0;
+
+        // Boucle de jeu
+        boolean reroll = false;
+        while (!reroll) {
+
+            // Affichage des dés
+            System.out.println("Lancé de dés : ");
+
+            // Pour chaque dé, on lance le dé et on affiche sa valeur
+            for (int i = 0; i < NB_DES; i++) {
+                // Si le dé n'est pas verrouillé, on le lance
+                if (!des[i].isLocked()) {
+                    des[i].roll();
+                    // On affiche la valeur du dé
+                    System.out.println("Dé " + (i + 1) + " : " + des[i].get_value());
+                } else {
+                    // Si le dé est verrouillé, on rappel sa valeur
+                    System.out.println("Le dé " + (i + 1) + " a été verrouillé durant les manches précédentes, sa valeur est : " + des[i].get_value());
+                }
+            }
+
+            // On demande à l'utilisateur s'il veut relancer un ou plusieurs dés
+            System.out.println("Voulez-vous relancer un ou plusieurs dés ? (o/n)");
+            char choice = Lire.c();
+
+            // Si l'utilisateur ne veut pas relancer de dés, on sort de la boucle
+            if (choice == 'n') reroll = true;
+            else {
+                // Sinon, on demande à l'utilisateur quels dés il veut relancer
+                for (int i = 0; i < NB_DES; i++) {
+
+                    // Si le dé est verrouillé, on passe à l'itération suivante
+                    if (des[i].isLocked()) continue;
+
+                    // Sinon, on demande à l'utilisateur s'il veut relancer le dé
+                    System.out.println("Voulez-vous relancer le dé " + (i + 1) + " ? (o/n)");
+                    choice = Lire.c();
+
+                    // Si l'utilisateur veut relancer le dé, on le relance
+                    if (choice == 'n') {
+                        des[i].setLocked(true);
+                        locked_dices++;
+                    }
+                }
+            }
+
+            // Si tous les dés sont verrouillés, on sort de la boucle
+            if (locked_dices == NB_DES) {
+                System.out.println("Vous avez verrouillé tous les dés, fin de la partie.");
+                reroll = true;
+            }
+
+            // On incrémente le score
+            score++;
+        }
+
+        // On retourne le score
+        return score;
+    }
+
+    /**
+     * Méthode pour initialiser les dés
+     */
+    public void initialize() {
+        // pour chaque dé, on crée un objet Dice et l'enregistre dans la liste
+        for (int i = 0; i < NB_DES; i++) {
+            this.des[i] = new Dice(faces);
+        }
+    }
 }
